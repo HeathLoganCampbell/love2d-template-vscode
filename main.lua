849627b240f80352554c7fe0d3b64227755e9836
Heath L Campbell <34885930+HeathLoganCampbell@users.noreply.github.com>
2025-01-25 12:53:47 +1100

    init

---
+++ main.lua
@@ -0,0 +1,19 @@
+-- Goes at top
+if arg[2] == "debug" then
+    require("lldebugger").start()
+end
+
+function love.draw()
+    love.graphics.print("Hello World", 400, 300)
+end
+
+-- Goes at botton
+local love_errorhandler = love.errorhandler
+
+function love.errorhandler(msg)
+    if lldebugger then
+        error(msg, 2)
+    else
+        return love_errorhandler(msg)
+    end
+end
---
+++ readme.md
@@ -0,0 +1,2 @@
+# How to build 
+Ctrl + Shift + B on VS Code
